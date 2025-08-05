e5f5a947d50aacfd08c3f805c79270b051ece3a8
fazm1 <farid.azzam.2001@gmail.com>
2025-08-05 13:54:09 +0300

    change 3

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/api', apiRoutes);
-
+console.log(process.env.CHROMA_URL);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
